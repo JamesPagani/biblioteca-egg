e482293a36ae5602df07b4da0b0474eccb56811a
JamesPagani <jaimeagalvezv@gmail.com>
2025-03-03 13:06:53 -0500

    Initial Commit
    By this point, this sample app has the model side of the app done.

---
+++ src/main/java/com/egg/biblioteca/servicios/AutorServicio.java
@@ -0,0 +1,58 @@
+package com.egg.biblioteca.servicios;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.egg.biblioteca.entidades.Autor;
+import com.egg.biblioteca.excepciones.MiException;
+import com.egg.biblioteca.repositorios.AutorRepositorio;
+
+@Service
+public class AutorServicio {
+    @Autowired
+    private AutorRepositorio autorRepositorio;
+
+    // EXTRAS
+    private void validar(String nombre) throws MiException {
+        if (nombre.isEmpty() || nombre == null) {
+            throw new MiException("El nombre no puede ser nulo o estar vacio!");
+        }
+    }
+
+    // CREATE
+    @Transactional
+    public void crearAutor(String nombre) throws MiException {
+        validar(nombre);
+        Autor autor = new Autor();
+        autor.setNombre(nombre);
+        autorRepositorio.save(autor);
+    }
+
+    // READ
+    @Transactional(readOnly = true)
+    public List<Autor> listarAutores() {
+        List<Autor> autores = new ArrayList<Autor>();
+
+        autores = autorRepositorio.findAll();
+
+        return autores;
+    }
+
+    // UPDATE
+    @Transactional
+    public void modificarAutor(String nombre, UUID id) throws MiException {
+        validar(nombre);
+        Optional<Autor> respuesta = autorRepositorio.findById(id);
+        if (respuesta.isPresent()) {
+            Autor autor = respuesta.get();
+            autor.setNombre(nombre);
+            autorRepositorio.save(autor);
+        }
+    }
+}
---
+++ src/main/java/com/egg/biblioteca/servicios/LibroServicio.java
@@ -0,0 +1,99 @@
+package com.egg.biblioteca.servicios;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.egg.biblioteca.entidades.Autor;
+import com.egg.biblioteca.entidades.Editorial;
+import com.egg.biblioteca.entidades.Libro;
+import com.egg.biblioteca.excepciones.MiException;
+import com.egg.biblioteca.repositorios.AutorRepositorio;
+import com.egg.biblioteca.repositorios.EditorialRepositorio;
+import com.egg.biblioteca.repositorios.LibroRepositorio;
+
+@Service
+public class LibroServicio {
+    @Autowired
+    private AutorRepositorio autorRepositorio;
+
+    @Autowired
+    private EditorialRepositorio editorialRepositorio;
+
+    @Autowired
+    private LibroRepositorio libroRepositorio;
+
+    // EXTRAS
+    private void validar(String titulo, Integer ejemplares, UUID idAutor, UUID idEditorial) throws MiException{
+        if (titulo.isEmpty() || titulo == null) {
+            throw new MiException("El titulo no puede estar vacio o ser nulo!");
+        }
+        if (ejemplares == null) {
+            throw new MiException("El numero de ejemplares no puede ser nulo!");
+        }
+        if (idAutor == null) {
+            throw new MiException("Debes proveer un id de Autor!");
+        }
+        if (idEditorial == null) {
+            throw new MiException("Debes proveer un id de Editorial!");
+        }
+    }
+
+    // CREATE
+    @Transactional
+    public void crearLibro(String titulo, Integer ejemplares, UUID idAutor, UUID idEditorial) throws MiException {
+        validar(titulo, ejemplares, idAutor, idEditorial);
+        Autor autor = autorRepositorio.findById(idAutor).get();
+        Editorial editorial = editorialRepositorio.findById(idEditorial).get();
+        Libro libro = new Libro();
+
+        libro.setTitulo(titulo);
+        libro.setEjemplares(ejemplares);
+        libro.setAlta(new Date());
+        libro.setAutor(autor);
+        libro.setEditorial(editorial);
+
+        libroRepositorio.save(libro);
+    }
+
+    // READ
+    @Transactional(readOnly = true)
+    public List<Libro> listarLibros() {
+        List<Libro> libros = new ArrayList<Libro>();
+        
+        libros = libroRepositorio.findAll();
+
+        return libros;
+    }
+
+    // UPDATE
+
+    @Transactional
+    public void modificarLibro(String titulo, Integer ejemplares, UUID idAutor, UUID idEditorial, Long idLibro) throws MiException {
+        validar(titulo, ejemplares, idAutor, idEditorial);
+        Optional<Libro> respuestaLibro = libroRepositorio.findById(idLibro);
+
+        if (respuestaLibro.isPresent()) {
+            Libro libro = respuestaLibro.get();
+            Optional<Autor> respuestaAutor = autorRepositorio.findById(idAutor);
+            Optional<Editorial> respuestaEditorial = editorialRepositorio.findById(idEditorial);
+
+            libro.setTitulo(titulo);
+            libro.setEjemplares(ejemplares);
+            if (respuestaAutor.isPresent()) {
+                libro.setAutor(respuestaAutor.get());
+            }
+            if (respuestaEditorial.isPresent()) {
+                libro.setEditorial(respuestaEditorial.get());
+            }
+
+            libroRepositorio.save(libro);
+        }
+    }
+}
